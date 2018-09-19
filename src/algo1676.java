bd2a766f12fbb2d06db418ff422a010833bb983f
623nana <6230nana@gmail.com>
2018-09-19 22:59:51 +0900

    1676_팩토리얼 0의 개수

---
+++ src/algo1676.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* 2018-09-19 */
+
+public class algo1676 {
+    public static void main(String [] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        int num = 0;
+
+        for(int i = 1; i<=n; i++){
+            num = i;
+            while(num%2 == 0 || num% 5 ==0) {
+                if (num % 2 == 0) num /= 2;
+                if (num % 5 == 0) {
+                    num /= 5;
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
