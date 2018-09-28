c9f3f6f04db14ab33fd00e18228c3e5ec65247b8
623nana <6230nana@gmail.com>
2018-09-28 18:35:34 +0900

    preTest_Q1다시수정

--- src/pretest/nhn_Q1.java
+++ src/pretest/nhn_Q1.java
@@ -10,27 +10,43 @@ public class nhn_Q1 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        /* 두번째 자리에 있는 수가 들어올 경우 예외 발생하기 때문에 큐 사용 X */
+        Queue<Integer> q = new LinkedList<>();
         ArrayList<Integer> list = new ArrayList<>();
+
         //유용..가운데 구분해주는거임..
         StringJoiner rslt = new StringJoiner("\n");
 
         String[] input = br.readLine().split(" ");
 
         for(String s : input){
-            if(list.size() != 3){
-                list.add(Integer.parseInt(s));
+            if(q.size() != 3){
+                q.offer(Integer.parseInt(s));
             } else {
-                if(list.contains(Integer.parseInt(s))){
-                    list.remove(Integer.valueOf(Integer.parseInt(s)));
-                    list.add(Integer.parseInt(s));
+                if(q.contains(Integer.parseInt(s))){
+                    // 값으로 삭제가 가능한가..? 암튼 된다..
+                    q.remove(Integer.parseInt(s));
+                    q.offer(Integer.parseInt(s));
                 } else {
-                    rslt.add(String.valueOf(list.get(0)));
-                    list.remove(0);
-                    list.add(Integer.parseInt(s));
+                    rslt.add(String.valueOf(q.poll()));
+                    q.offer(Integer.parseInt(s));
                 }
             }
         }
+
+//        for(String s : input){
+//            if(list.size() != 3){
+//                list.add(Integer.parseInt(s));
+//            } else {
+//                if(list.contains(Integer.parseInt(s))){
+//                    list.remove(Integer.valueOf(Integer.parseInt(s)));
+//                    list.add(Integer.parseInt(s));
+//                } else {
+//                    rslt.add(String.valueOf(list.get(0)));
+//                    list.remove(0);
+//                    list.add(Integer.parseInt(s));
+//                }
+//            }
+//        }
 
         System.out.println(rslt.length() == 0 ? 0 : rslt.toString());
     }
